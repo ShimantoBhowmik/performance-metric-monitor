40fa90924dd0dcc080156bb48fe4e6a21f1a988b
Shimanto Bhowmik <bmikshimanto@gmail.com>
2023-10-07 18:36:33 -0400

    Added code to start and kill processes for the project

--- project1.sh
+++ project1.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+pids=()
+start_processes() {
+    processes=("APM1" "APM2" "APM3" "APM4" "APM5" "APM6")
+    for process in "${processes[@]}"; do
+        ./"$process" "$1" &
+        pid=$!
+        pids+=("$pid")
+        echo "Started $process: $pid"
+    done
+
+    ifstat -a -d 1
+    ifstat_pid=$!
+    pids+=("$ifstat_pid")
+}
+
+
 cleanup(){
-    kill -9 $pid1
-	kill -9 $pid2 
-	kill -9 $pid3
-	kill -9 $pid4
-	kill -9 $pid5
-	kill -9 $pid6
-	#more to follow
+    for pid in "${pids[@]}"; do
+        kill -9 "$pid"
+        echo "Killed process: $pid"
+    done
+    exit $?
 }
