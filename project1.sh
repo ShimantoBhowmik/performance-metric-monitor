7d10c4248fe6203a0cee94e244d02a1eae7b36e6
Shimanto Bhowmik <bmikshimanto@gmail.com>
2023-10-07 22:05:34 -0400

    Included Main Function for the script

--- project1.sh
+++ project1.sh
@@ -15,11 +15,29 @@ start_processes() {
     pids+=("$ifstat_pid")
 }
 
-
 cleanup(){
     for pid in "${pids[@]}"; do
         kill -9 "$pid"
         echo "Killed process: $pid"
     done
     exit $?
+}
+
+main(){
+    #trap ctrl-c and call cleanup function
+    trap cleanup SIGINT
+    #get IP address from user
+    read -p "Please enter the IP address: " IP
+    start_processes "$IP"
+    SECONDS = 0;
+    while true; do
+        sleep 5;
+        echo "Skipping 5s"
+		total_time=$SECONDS
+		if [[ $total_time -ge 900 ]]; then
+			cleanup
+		fi
+		#make and call process metrics
+        #make and call system metrics
+    done
 }
